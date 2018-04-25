158b258413dcdd496f976277e2d236d2a8a21ebe
Muhammad Yusuf <mail.secondson@gmail.com>
2018-04-25 11:00:45 +0700

    Membuat Project Spring Boot

---
+++ src/main/java/com/secondson/springbootNew/controller/Hello.java
@@ -0,0 +1,16 @@
+package com.secondson.springbootNew.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+
+	@GetMapping("/hello")
+	public String Hello() {
+		return "Hello";
+	}
+	
+}
